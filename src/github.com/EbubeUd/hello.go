1665038a9915f9346c8d899b4977feb36a375f9c
kripsonud <kripsonud@gmail.com>
2021-03-31 09:26:27 +0100

    Enumerated constants and operations using iota

--- src/github.com/EbubeUd/hello.go
+++ src/github.com/EbubeUd/hello.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 )
 
-func main() {
-	var n complex64 = 1 + 2i
-	var j complex64 = complex(1, 2)
-	var r rune = 'a'
+const (
+	_ = iota
+	x
+	y
+	z
+)
 
-	fmt.Printf("%v and %T\n", real(n), n)
-	fmt.Printf("%v and %T\n", imag(j), j)
-	fmt.Printf("%v and %T\n", r, r)
+func main() {
+
+	fmt.Printf("%v", x)
 }
