4648b436aec1817be7485ba2c507ff2f880ef4d2
kripsonud <kripsonud@gmail.com>
2021-03-31 08:14:47 +0100

    Test Int To string Conversion

--- src/github.com/EbubeUd/hello.go
+++ src/github.com/EbubeUd/hello.go
@@ -1,12 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+var hello string = "local/private"
+var Hello string = "Public"
 
 func main() {
-	var i float32 = 20
+	var i float32 = 20.
 	k := 9
+
+	k = int(i)
+	hello = strconv.Itoa(k)
 
 	fmt.Println(i)
 	fmt.Printf("%v is %T", i, i)
 	fmt.Printf("%v is %T", k, k)
+	fmt.Printf("%v is %T", hello, hello)
 }
