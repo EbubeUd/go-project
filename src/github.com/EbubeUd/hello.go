5245d78891ecd849151f0190e93d5c01f06f826d
kripsonud <kripsonud@gmail.com>
2021-03-31 21:30:32 +0100

    Conditionals

--- src/github.com/EbubeUd/hello.go
+++ src/github.com/EbubeUd/hello.go
@@ -1,55 +1,18 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-)
-
-type Doctor struct {
-	User
-	age         uint
-	isQualified bool
-}
-
-type User struct {
-	name  string `required max:"100"`
-	email string
-}
+import "fmt"
 
 func main() {
 
-	people := map[string]uint8{
-		"Hello": 1,
-		"World": 2,
+	if false {
+		fmt.Println("Hello")
 	}
 
-	places := make(map[string]uint8)
-
-	places = map[string]uint8{
-		"Enugu": 1,
-		"Delta": 2,
+	switch 6 {
+	case 1, 2, 3, 4, 5:
+		fmt.Println("true")
+	default:
+		fmt.Println("false")
 	}
-
-	delete(places, "Enugu")
-	a := len(people)
-
-	_, out := places["Delta"]
-
-	aDoc := Doctor{}
-
-	aDoc.name = "Ebube"
-	aDoc.age = 12
-	aDoc.email = "kripsonud@gmail.com"
-	aDoc.isQualified = true
-
-	t := reflect.TypeOf(User{})
-	field, _ := t.FieldByName("name")
-
-	fmt.Printf("%+v\n", people["Hello"])
-	fmt.Printf("%+v\n", a)
-	fmt.Printf("%+v\n", places)
-	fmt.Printf("%+v\n", out)
-	fmt.Printf("%+v\n", aDoc)
-	fmt.Printf("%+v\n", field)
 
 }
